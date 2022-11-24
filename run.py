e6eae582aa0f61c083499249b9aec92c76692479
Arminas19 <armeska14@gmail.com>
2022-11-24 14:19:09 +0000

    added comments for my future-self.

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-# use flask run to run server
+# use 'flask run' to run server
+# video on 31:14 / video link - https://www.youtube.com/watch?v=qbLc5a9jdXo
 @app.route('/')
 def index():
     return 'Hello!'
